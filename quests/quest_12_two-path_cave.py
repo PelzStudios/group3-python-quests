7006410bee626f4772a7ff35deb0d2b86d25c03e
Sostene Uwihanganye <usteensoostain@gmail.com>
2026-08-28 08:07:31 +0400

    quest 12 done two-path cave

---
+++ quests/quest_12_two-path_cave.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+realPassword="@binbash12"
+password=input("Enter your password here: ")
+if password == realPassword:
+    print("Access Granted!")
+else:
+    print("Access Denied!")
